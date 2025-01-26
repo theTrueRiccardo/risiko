b38ef8a69e9988f99bbd67795b63515579e83705
Riccardo Bova <ric.bova@gmail.com>
2025-01-26 19:31:58 +0100

    refined build script

--- build.sh
+++ build.sh
@@ -4,4 +4,11 @@ fi
 mkdir ./bin
 find ./src -name "*.java" | sed '/\.\/src\/test.*/d' > sources.txt
 javac -classpath ./src -d ./bin @./sources.txt
+rm ./sources.txt
 cp -r ./resources/. ./bin
+if [ -d "./deploy" ]; then
+  rm -r ./deploy
+fi
+mkdir ./deploy
+jar cfm deploy/risiko-client.jar ./manifest-client.txt -C ./bin client -C ./bin comune -C ./bin utilita
+jar cfm deploy/risiko-server.jar ./manifest-server.txt -C ./bin server -C ./bin comune -C ./bin utilita
